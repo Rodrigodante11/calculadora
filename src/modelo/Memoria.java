ae412fdf197e5e62cd8bc472e040b69404b72f7a
Rodrigo Augusto <rodrigoaugusto839@gmail.com>
2021-04-27 13:21:00 -0300

    Logica da calculadora

--- src/modelo/Memoria.java
+++ src/modelo/Memoria.java
@@ -4,9 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Memoria {
+	
+	private enum TipoComando  {
+		ZERAR , NUMERO , DIV ,MULT ,SUB , SOMA ,IGUAL , VIRGULA
+	};
+	
 	private static final   Memoria instancia = new Memoria();
 	
+	private TipoComando ultimaOperacao = null;
+	private boolean subtituir = false;
 	private String textoAtual = "";
+	private String textoBuffer = "";
 	private final List<MemoriaObservador> observadores = new ArrayList<>();
 	
 	private Memoria() {
@@ -25,13 +33,61 @@ public class Memoria {
 		observadores.add(observador);
 	}
 	
-	public void processarComando(String valor) {
-		if("AC".equals(valor)) {
-			textoAtual =""; //para limpar o campo
-		}else {
-			textoAtual+=valor;
+	public void processarComando(String texto) {
+		
+
+		TipoComando tipoComando = detectarTipoComando(texto);
+		
+		if(tipoComando ==null) {
+			return;
+		}else if(tipoComando == TipoComando.ZERAR) {
+			textoAtual ="";
+			textoBuffer = "";
+			subtituir = false;
+			ultimaOperacao = null;
+		}else if(tipoComando == TipoComando.NUMERO || tipoComando ==TipoComando.VIRGULA) {
+			textoAtual =subtituir ? texto : textoAtual+texto;
+			subtituir = false;
 		}
 		
 		observadores.forEach(o -> o.valorAlternado(textoAtual));
+	}
+
+	private TipoComando detectarTipoComando(String texto) {
+		
+		if(textoAtual.isEmpty() && texto =="0") {
+			return null;
+		}
+		try {
+			Integer.parseInt(texto); //tranforemado em numero
+			return TipoComando.NUMERO;
+			
+		} catch (NumberFormatException e) {
+			
+			//Quando não for numero
+			if("AC".equals(texto)) {
+				return TipoComando.ZERAR;
+				
+			}else if("/".equals(texto)){
+				return TipoComando.DIV;
+				
+			}else if("*".equals(texto)){
+				return TipoComando.MULT;
+				
+			}else if("-".equals(texto)){
+				return TipoComando.SUB;
+				
+			}else if(",".equals(texto) && !textoAtual.contains(",")){
+				return TipoComando.VIRGULA;
+				
+			}else if("+".equals(texto)){
+				return TipoComando.SOMA;
+				
+			}else if("=".equals(texto)){
+				return TipoComando.IGUAL;
+			}
+		}
+		return null;
+		
 	}
 }
